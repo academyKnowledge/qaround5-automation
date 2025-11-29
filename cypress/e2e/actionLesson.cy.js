bb2a80340d004582afdab49bde9887696e4c5b13
EngRazanBalatiah <eng.razanBalatiah96@gmail.com>
2025-11-29 19:47:42 +0200

    actions part 2

--- cypress/e2e/actionLesson.cy.js
+++ cypress/e2e/actionLesson.cy.js
@@ -20,10 +20,40 @@ describe('Actions', () => {
         cy.get("[name=q]").type("test{enter}")
     });
 
-    it.only('Select command', () => {
+    it('Select command', () => {
         cy.visit("http://127.0.0.1:5500/actionsLesson.html")
         cy.get("#country").select("Jordan")
         cy.get("#country").select("egypt_country")
         cy.get("#country").select(0)
+    });
+
+    it('check uncheck commands', () => {
+        cy.visit("http://127.0.0.1:5500/actionsLesson.html")
+        // cy.get("#Banana").check()
+        // cy.get("[name=snack4]").check().uncheck()
+        cy.get("[type=checkbox]").check(['Banana','Fries','le'])
+        cy.get("[type=checkbox]").uncheck('Fries')
+        cy.get("[type=checkbox]").uncheck(['Banana','le'])
+        cy.get("#developer").check()
+        // 
+       // cy.get("#tester").check() 
+        cy.reload()
+        cy.get("#checkMe").check({force:true})
+    });
+
+    it('dbclick command', () => {
+        cy.visit("http://127.0.0.1:5500/actionsLesson.html")
+        cy.get("#dbClick").dblclick()
+    });
+
+    it('focus and blur commands', () => {
+       cy.visit("http://127.0.0.1:5500/actionsLesson.html")
+       cy.get("#myTextField").focus().blur()
+    });
+
+    it.only('trigger command', () => {
+      cy.visit("http://127.0.0.1:5500/actionsLesson.html")
+        cy.get("#trigger_btn").trigger("mouseover").trigger("mouseleave")
+        cy.get("#btn1").trigger("mousedown")
     });
 });
